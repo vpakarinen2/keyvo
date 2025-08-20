84ce6d523db10cb2dcfdbdf8cf5db06a86f679e9
Ville Pakarinen <151925563+vpakarinen2@users.noreply.github.com>
2025-08-20 13:11:37 +0300

    Added app main layout file

---
+++ keyvo-frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google"
+import type { Metadata } from "next"
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Keyvo",
+  description: "Long-tail keyword suggestion tool",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
